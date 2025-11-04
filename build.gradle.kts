4f41add229db2cb40963a0f84a0f053c77110ef6
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2025-11-04 21:13:10 +0100

    Update: data

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     api("dev.slne.surf:surf-database:2.2.1-SNAPSHOT")
 }
 group = "de.hiorcraft.nex"
-version = "1.21.10-1.0.0"
+version = "1.21.10-1.2.0"
 
 surfPaperPluginApi {
     mainClass("de.hiorcraft.nex.nextools.BukkitMain")
