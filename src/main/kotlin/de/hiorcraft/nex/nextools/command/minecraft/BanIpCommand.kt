29b89ef4c2d9e7651ac2c393665b4a11b637b295
HiorCraft <200645597+HiorCraft@users.noreply.github.com>
2025-11-25 15:33:08 +0100

    Feat: uitgeschakeld Ban and Ban-Ip Command

---
+++ src/main/kotlin/de/hiorcraft/nex/nextools/command/minecraft/BanIpCommand.kt
@@ -0,0 +1,30 @@
+package de.hiorcraft.nex.nextools.command.minecraft
+
+import de.hiorcraft.nex.nextools.util.PermissionRegistry
+import dev.jorel.commandapi.kotlindsl.anyExecutor
+import dev.jorel.commandapi.kotlindsl.commandAPICommand
+import dev.jorel.commandapi.kotlindsl.entitySelectorArgumentOnePlayer
+import dev.jorel.commandapi.kotlindsl.getValue
+import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import org.bukkit.entity.Player
+
+fun BanIpCommand() = commandAPICommand("ban-ip") {
+    withPermission(PermissionRegistry.COMMAND_BANIP)
+
+    anyExecutor { executor, _ ->
+        executor.sendText {
+            appendPrefix()
+            error("Der Ban-Befehl wurde deaktiviert.")
+        }
+    }
+
+    entitySelectorArgumentOnePlayer("player") {
+        anyExecutor { executor, args ->
+            val player: Player by args
+            executor.sendText {
+                appendPrefix()
+                error("Der Ban-Befehl wurde deaktiviert.")
+            }
+        }
+    }
+}
