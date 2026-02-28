d27366a4c6120a690fbdde7f27c61711862870bf
HiorCraft <HiorCraft@users.noreply.github.com>
2026-03-01 00:04:19 +0100

    Feat: Update Test Command

--- src/main/kotlin/de/hiorcraft/nex/nextools/command/TestCommand.kt
+++ src/main/kotlin/de/hiorcraft/nex/nextools/command/TestCommand.kt
@@ -2,15 +2,40 @@ package de.hiorcraft.nex.nextools.command
 
 import de.hiorcraft.nex.nextools.util.PermissionRegistry
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
+import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
-import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.surfapi.core.api.messages.adventure.buildText
 
 fun testCommand() = commandAPICommand("test") {
     withPermission(PermissionRegistry.COMMAND_TEST)
-    playerExecutor { player , args ->
-        player.sendText {
-            appendInfoPrefix()
-            info("Dies ist ein Testbefehl.")
+
+    playerExecutor { player, _ ->
+        player.sendMessage("Hier sind paar Testnachrichten:")
+    }
+
+    literalArgument("Ranks") {
+        playerExecutor { player, args ->
+            buildText {
+                spacer("-------------------------------")
+                info("Admin = \uE000")
+                spacer("")
+                info("Developer = \uE002")
+                spacer("")
+                info("Moderator = \uE004")
+                spacer("")
+                info("supporter = \uE007")
+                spacer("")
+                info("Vip = \uE008")
+                spacer("")
+                info("Content Creator = \uE001")
+                spacer("")
+                info("Premium+ = \uE006")
+                spacer("")
+                info("Premium = \uE005")
+                spacer("")
+                info("Member = \uE003")
+                spacer("-------------------------------")
+            }
         }
     }
 }
